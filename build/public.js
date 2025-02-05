6a1903c7df3be8a24e9ef28125d8c1f4667f0941
Luciano <lucianorepetto07@gmail.com>
2025-02-05 16:01:19 -0300

    public src

--- build/public.js
+++ build/public.js
@@ -1,32 +1,14 @@
 //node build/public.js
-const execProcess = require("./exec_process.js");
 
-function runCommand(command, callback) {
-    execProcess.result(command, function (err, response) {
-        if (err) {
-            console.log(`Error ejecutando "${command}":`, err);
-        } else {
-            console.log(`Salida de "${command}":`, response);
+function public(){
+    //ejecutamos la subida de fotos en git
+    var execProcess = require("./exec_process.js");
+    execProcess.result("git add . && git commit -m \"public src\" && git push -u origin main", function(err, response){
+        if(!err){
+            console.log(response);
+        }else {
+            console.log(err);
         }
-        callback(err);
-    });
-}
-
-function public() {
-    runCommand("git add index.html", (err) => {
-        if (err) return;
-        runCommand("git add .\\assets\\", (err) => {
-            if (err) return;
-            runCommand("git add .\\styles\\", (err) => {
-                if (err) return;
-                runCommand('git commit -m "public"', (err) => {
-                    if (err) return;
-                    runCommand("git push -u origin main", (err) => {
-                        if (err) console.log("Error al hacer push:", err);
-                    });
-                });
-            });
-        });
     });
 }
 
