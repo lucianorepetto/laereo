3db7e3ba0df733ec69029619825194a0800d358b
Luciano <lucianorepetto07@gmail.com>
2025-02-13 16:05:18 -0300

    public 13/02/2025 16:05

--- scripts/galeria.js
+++ scripts/galeria.js
@@ -3,12 +3,11 @@ function fullScreenGallery(images, clickedSrc) {
     
     let overlay = $('<div>', { class: 'sliderFullScreen-overlay' }).appendTo('body');
     let prevBtn = $('<div>', { class: 'sliderFullScreen-nav sliderFullScreen-prev', text: '<' }).appendTo(overlay);
-    let carousel = $('<div>', { class: 'sliderFullScreen-carousel' }).appendTo(overlay);
+    let carousel = $('<div>', { class: 'sliderFullScreen-carousel invisible-scroll' }).appendTo(overlay);
     
     images.forEach(src => {
         $('<img>', { class: 'sliderFullScreen-carousel-img' }).attr('src', src).appendTo(carousel);
     });
-    
     
     let nextBtn = $('<div>', { class: 'sliderFullScreen-nav sliderFullScreen-next', text: '>' }).appendTo(overlay);
     let closeBtn = $('<div>', { class: 'sliderFullScreen-close', text: '×' }).appendTo(overlay);
@@ -16,6 +15,9 @@ function fullScreenGallery(images, clickedSrc) {
     function updatePosition() {
         $('.sliderFullScreen-carousel-img').removeClass('active');
         $('.sliderFullScreen-carousel-img').eq(currentIndex).addClass('active');
+        
+        // Desplazamos el carrusel para hacer que la imagen activa esté centrada
+        carousel.scrollLeft($('.sliderFullScreen-carousel-img').eq(currentIndex)[0].offsetLeft);
     }
     
     updatePosition();
@@ -40,21 +42,15 @@ function fullScreenGallery(images, clickedSrc) {
         }
     });
     
-    // Swipe gesture support
-    let startX = 0;
-    
-    carousel.on('touchstart', function (e) {
-        startX = e.originalEvent.touches[0].clientX;
-    });
-    
-    carousel.on('touchend', function (e) {
-        let endX = e.originalEvent.changedTouches[0].clientX;
-        if (startX - endX > 50) {
-            currentIndex = (currentIndex + 1) % images.length;
-        } else if (endX - startX > 50) {
-            currentIndex = (currentIndex - 1 + images.length) % images.length;
+    // Manejo del desplazamiento con la rueda del mouse
+    carousel.on('wheel', function (e) {
+        if (e.originalEvent.deltaY > 0) {
+            currentIndex = (currentIndex + 1) % images.length; // Next image
+        } else {
+            currentIndex = (currentIndex - 1 + images.length) % images.length; // Previous image
         }
         updatePosition();
+        e.preventDefault(); // Prevenir el comportamiento por defecto del scroll
     });
 
     let resize = function() {
@@ -68,7 +64,6 @@ function fullScreenGallery(images, clickedSrc) {
             carousel.css('width', '');  // Aquí puedes definir el valor original si lo necesitas
         }
     }
-
 
     // Hide arrows on mobile
     $(window).resize(()=>{resize()});
